1342880f0090d14ae127a37987fc035bea058d0d
uldisn <uldisnelsons@gmail.com>
2021-04-14 20:23:23 +0300

    BrowserDataHelper fixed bug
    Signed-off-by: uldisn <uldisnelsons@gmail.com>

--- logic/BrowserDataHelper.php
+++ logic/BrowserDataHelper.php
@@ -13,19 +13,20 @@ class BrowserDataHelper
     public static function data(): array
     {
         $Browser = new BrowserDetection();
-        $useragent = $_SERVER['HTTP_USER_AGENT'];
-        $data = ArrayHelper::filter(
-            $Browser->getAll($useragent), [
+        if($useragent = $_SERVER['HTTP_USER_AGENT']??false) {
+            $data = ArrayHelper::filter(
+                $Browser->getAll($useragent), [
 //                'os_type',
 //                'os_family',
 //                'os_name',
 //                'os_version',
-            'os_title',
-            'device_type',
+                'os_title',
+                'device_type',
 //                'browser_name',
 //                'browser_version',
-            'browser_title'
-        ]);
+                'browser_title'
+            ]);
+        }
         $data['userIp'] = Yii::$app->request->getUserIP();
         return $data;
     }
