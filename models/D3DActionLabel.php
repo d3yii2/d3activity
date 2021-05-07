eac38befb6e7e79a2a007b3e48a55428fa79eb66
VairisO <vairis.ozo@gmail.com>
2021-05-07 03:23:14 +0300

    Add files via upload

---
+++ models/D3DActionLabel.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace d3yii2\d3activity\models;
+
+use \d3yii2\d3activity\models\base\D3DActionLabel as BaseD3DActionLabel;
+
+/**
+ * This is the model class for table "d3d_action_label".
+ */
+class D3DActionLabel extends BaseD3DActionLabel
+{
+    public function afterSave($insert, $changedAttributes)
+    {
+        parent::afterSave($insert, $changedAttributes);
+        D3aActionLabelDictionary::clearCache();
+    }
+
+    public function afterDelete()
+    {
+        parent::afterDelete();
+        D3aActionLabelDictionary::clearCache();
+    }
+}
