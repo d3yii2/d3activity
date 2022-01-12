f8fa631923734f3ec18b83439eaf8afe0594a449
vairis <vairis@webhaus.lv>
2022-01-12 03:41:22 +0200

    - fix

--- components/DailyActivityNotification.php
+++ components/DailyActivityNotification.php
@@ -6,6 +6,7 @@ use d3system\commands\D3CommandController;
 use d3yii2\d3activity\models\D3aActivity;
 use d3yii2\d3activity\models\D3aLastNotification;
 use d3yii2\d3pop3\components\D3Mail;
+use yii\console\ExitCode;
 use yii\validators\EmailValidator;
 use Yii;
 
@@ -20,6 +21,7 @@ use Yii;
  *  'dailyActivityNotification' =>[
  *      'class' => 'd3yii2\components\DailyActivityNotification',
  *      'sysCompanyIds' => [1 => 'email1@email1.com', 2 => 'email2@email2.com', 3 => ....],
+ *      'sysModelIds' => [1,2,3,4,5]
  *  ]
  */
 
@@ -28,13 +30,16 @@ class DailyActivityNotification extends D3CommandController {
     /** @var array */
     public $sysCompanyIds = [];
 
+    /** @var array */
+    public $sysModelIds = [];
+
     /** @var string */
     public $subject = 'Daily System Notification';
 
     /** @var string */
     public $fromEmail = 'info@system.com';
 
-    public function actionIndex(): void
+    public function actionIndex() : int
     {
         if(!empty($this->sysCompanyIds)) {
             foreach ($this->sysCompanyIds as $companyId => $email) {
@@ -42,58 +47,63 @@ class DailyActivityNotification extends D3CommandController {
                 if(empty($companyId) || empty($email) || !$emailValidator->validate($email) || !is_int($companyId)) {
                     continue;
                 }
-               if($lastNotifications =  D3aLastNotification::find(['sys_company_id'])->orderBy(['time' => SORT_DESC])->one()) {
-                   if($newActivities =  D3aActivity::find(['sys_company_id' => $companyId])->andWhere(['>', 'time', $lastNotifications->time])->all()) {
-                        if($this->composeEmail($newActivities, $email)) {
+               if($lastNotifications =  D3aLastNotification::find()->where(['sys_company_id' => $companyId])->orderBy(['time' => SORT_DESC])->one()) {
+                   if($newActivities =  D3aActivity::find()->where(['sys_company_id' => $companyId])->andWhere(['sys_model_id' => $this->sysModelIds])->andWhere(['>', 'time', $lastNotifications->time])->all()) {
+                        if($this->composeEmail($newActivities, $email, $companyId)) {
                             $this->logSentActivities($companyId);
                         }
                    }
                } else {
-                   if($newActivities =  D3aActivity::find(['sys_company_id' => $companyId])->all()) {
-                       if($this->composeEmail($newActivities, $email)) {
+
+                   if($newActivities =  D3aActivity::find()->where(['sys_company_id' => $companyId])->andWhere(['sys_model_id' => $this->sysModelIds])->all()) {
+                       if($this->composeEmail($newActivities, $email, $companyId)) {
                            $this->logSentActivities($companyId);
                        }
                    }
                }
             }
 
-            echo 'report complete';
+            return ExitCode::OK;
         }
 
-        echo 'sysCompanyIds empty';
+        return ExitCode::NOINPUT;
     }
 
-    private function composeEmail(array $newActivities, string $emailTo): bool
+    private function composeEmail(array $newActivities, string $emailTo, int $companyId): bool
     {
-        $connection  = Yii::$app->db;
+        $connection  = $this->getConnection();
         $transaction = $connection->beginTransaction();
         try {
             $email = new D3Mail();
             $html = Yii::$app->controller->renderPartial('@d3yii2/d3activity/views/email/dailyActivityNotification',['activities' => $newActivities]);
             $email->setBodyHtml($html)
+                ->setEmailId(['SYS', $companyId, 'INV', $newActivities[0]->id, date('YmdHis')])
                 ->setSubject($this->subject)
                 ->setFromEmail($this->fromEmail)
-                ->addAddressTo($emailTo);
-            if($email->save()) {
+                ->addAddressTo($emailTo)
+                ->addSendReceiveToInCompany($companyId)
+                ->save();
+
                 $email->send();
-            }
-            return true;
+                $transaction->commit();
+                return true;
         } catch (\Exception $e) {
             Yii::error($e->getMessage());
             Yii::error($e->getTraceAsString());
             $transaction->rollback();
-            return false;
         }
+        return false;
     }
 
     private function logSentActivities(int $sysCompanyId): void
     {
-        $connection  = Yii::$app->db;
+        $connection  = $this->getConnection();
         $transaction = $connection->beginTransaction();
         try {
             $model = new D3aLastNotification();
             $model->sys_company_id = $sysCompanyId;
             $model->save();
+            $transaction->commit();
         } catch (\Exception $e) {
             Yii::error($e->getMessage());
             Yii::error($e->getTraceAsString());
--- views/email/dailyActivityNotification.php
+++ views/email/dailyActivityNotification.php
@@ -1,5 +1,7 @@
 <?php
 use d3yii2\d3activity\dictionaries\D3aActionLabelDictionary;
+use yii2d3\d3persons\models\D3pPerson;
+use yii2d3\d3persons\models\User;
 /**
  * @var \yii\data\ActiveDataProvider $activities
  */
@@ -7,16 +9,28 @@ use d3yii2\d3activity\dictionaries\D3aActionLabelDictionary;
 
 ?>
 
-<div class="row file-row">
-        <table style="table-layout: auto">
+<div class="">
+        <table class="report_table">
             <thead>
             <th>Name</th>
+            <th>User </th>
             <th>Date Time</th>
             </thead>
             <tbody>
             <?php foreach ($activities as $a) { ?>
                 <tr>
                     <td><?= D3aActionLabelDictionary::getLabel($a->action_id) ?></td>
+                    <td><?php
+                        if($user = User::findOne($a->user_id)) {
+                            if($person = D3pPerson::findOne(['user_id' => $a->user_id])) {
+                                echo $person->getFullName();
+                            } else {
+                                echo $user->username;
+                            }
+                        } else {
+                            echo '-';
+                        }
+                        ?></td>
                     <td><?= $a->time?></td>
                 </tr>
             <?php } ?>
@@ -25,4 +39,16 @@ use d3yii2\d3activity\dictionaries\D3aActionLabelDictionary;
 
 </div>
 
+<style>
+    .report_table {
+        table-layout: auto; width:100%;
+    }
 
+    .report_table td {
+        border: 1px solid black;
+        padding: 2px 5px;
+    }
+    .report_table thead {
+        background: #d0d0d0;
+    }
+</style>
